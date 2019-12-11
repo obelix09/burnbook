0c900b3bba20dfdd7f24579689dcd26a45dadd95
Arna Jónasardóttir <arnaj17@ru.is>
2019-12-11 19:06:47 +0000

    getallgosip

--- services/burnbookService.js
+++ services/burnbookService.js
@@ -25,11 +25,31 @@ const burnbookService = () => {
         });
         console.log("wtf is this " + dbname)
         console.log("results are in" + result)
-        return result
+        return result;
+    }
+
+    const getAllGossip = () => {
+        let gossips;
+        const connection = MongoClient.connect(connectionString, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }, function(err, client) {
+            if(err) {throw new Error(err);}
+            const db = client.db('BurnBook');
+            gossips = db.collection('gossips')
+            gossips.find({}).toArray(function(err, items) {
+                if(err) {throw new Error(err); }
+                console.log("*** ITEMS: **** " + items);
+            });
+            client.close();
+        });
+        console.log("results are in " + gossips)
+        return gossips;
     }
 
     return { 
-        getGossipByName
+        getGossipByName,
+        getAllGossip
     };
 };
 
