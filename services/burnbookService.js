9362939257a9f3a8d532ff2ab0276ba493323636
Arna Jónasardóttir <arnaj17@ru.is>
2019-12-12 11:53:24 +0000

    all gossip appears on page

--- services/burnbookService.js
+++ services/burnbookService.js
@@ -28,28 +28,8 @@ const burnbookService = () => {
         return result;
     }
 
-    const getAllGossip = () => {
-        let gossips;
-        const connection = MongoClient.connect(connectionString, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        }, function (err, client) {
-            if (err) { throw new Error(err); }
-            const db = client.db('BurnBook');
-            gossips = db.collection('gossips')
-            gossips.find({}).toArray(function (err, items) {
-                if (err) { throw new Error(err); }
-                console.log("*** ITEMS: **** " + items);
-            });
-            client.close();
-        });
-        console.log("results are in " + gossips)
-        return gossips;
-    }
-
     return {
-        getGossipByName,
-        getAllGossip
+        getGossipByName
     };
 };
 
